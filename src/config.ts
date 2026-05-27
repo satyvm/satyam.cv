dc4d101430294b77e6b361ac786fe72d2f31bf54
Satyam <75127014+satyvm@users.noreply.github.com>
2026-05-27 13:20:17 +0530

    update: site configuration and footer content

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://chiri.the3ash.com/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    website: 'https://satyvm.com/', // Site domain
+    title: 'Satyam', // Site title
+    author: 'satyam', // Author name
+    description: 'blog of satyam', // Site description
     language: 'en-US' // Default language
   },
 
@@ -14,7 +14,7 @@ export const themeConfig: ThemeConfig = {
   general: {
     contentWidth: '35rem', // Content area width
     centeredLayout: true, // Use centered layout (false for left-aligned)
-    themeToggle: false, // Show theme toggle button (uses system theme by default)
+    themeToggle: true, // Show theme toggle button (uses system theme by default)
     postListDottedDivider: false, // Show dotted divider in post list
     footer: true, // Show footer
     fadeAnimation: true // Enable fade animations
@@ -22,18 +22,17 @@ export const themeConfig: ThemeConfig = {
 
   // DATE SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   date: {
-    dateFormat: 'YYYY-MM-DD', // Date format: YYYY-MM-DD, MM-DD-YYYY, DD-MM-YYYY, MONTH DAY YYYY, DAY MONTH YYYY
+    dateFormat: 'DD-MM-YYYY', // Date format: YYYY-MM-DD, MM-DD-YYYY, DD-MM-YYYY, MONTH DAY YYYY, DAY MONTH YYYY
     dateSeparator: '.', // Date separator: . - / (except for MONTH DAY YYYY and DAY MONTH YYYY)
     dateOnRight: true // Date position in post list (true for right, false for left)
   },
 
   // POST SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   post: {
-    readingTime: false, // Show reading time in posts
+    readingTime: true, // Show reading time in posts
     toc: true, // Show table of contents (when there is enough page width)
     imageViewer: true, // Enable image viewer
     copyCode: true, // Enable copy button in code blocks
-    linkCard: true, // Enable link card
-    katex: true // Enable KaTeX math rendering
+    linkCard: true // Enable link card
   }
 }
--- src/content/about/about.md
+++ src/content/about/about.md
@@ -7,10 +7,12 @@ This content will be displayed at the top of the index page.
 You can leave this empty if you don’t want to show any content.
 -->
 
-A static blog theme based on [Astro](https://astro.build), _designed for clarity and focus_.
+Crafting infra. _Open to work_. Contributing to open source and learning for CKA.
 
-With a deliberately minimal design, this layout ensures your content takes center stage. It's built for flexibility, offering customization options that honor its clean and elegant aesthetic.
+I have spent my college days at IITM, participating in hackathons. After graduating in 2024 with a B.Tech. in ocean engg, I worked for a year at a hedge fund and a fintech startup. Explored areas of crypto, finance, and programming. There, I decided to focus on connecting hardware and software, and now I enjoy my time learning while transitioning into a new career.
 
-Effortlessly share your thoughts in a calm & dustless space.
+All I want to do is build infra where users, devs, and AI rely on and be part of something meaningful.
 
-Check posts for details and view source on [GitHub](https://github.com/the3ash/astro-chiri).
+In my free time, I like to hoop, play valorant, and listen to music.
+
+Connect me at [x](https://x.com/satyvm), [linkedin](linkedin.com/in/satyvm), and [mail](mailto:hi@satyvm.com).
